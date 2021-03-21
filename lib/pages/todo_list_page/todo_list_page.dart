19d5a5b60c86607b7ea654a1b50723d8156242a8
HironoriSato <maza.hs5360@gmail.com>
2021-03-21 20:39:15 +0900

    [add]todo削除機能の実装

--- lib/pages/todo_list_page/todo_list_page.dart
+++ lib/pages/todo_list_page/todo_list_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo/model/controllers/controllers.dart';
+import 'package:flutter_todo/model/entities/entities.dart';
 import 'package:flutter_todo/pages/todo_upsert_page/todo_upsert_page.dart';
 import 'package:flutter_todo/pages/todo_upsert_page/todo_upsert_page_arg.dart';
 import 'package:flutter_todo/providers/providers.dart';
+import 'package:flutter_todo/widgets/confirm_dialog.dart';
+import 'package:flutter_todo/widgets/widgets.dart';
 
 import 'tile/todo_tile.dart';
 
@@ -30,14 +33,64 @@ class TodoListPage extends ConsumerWidget {
 }
 
 class _ListView extends ConsumerWidget {
+  Future<bool> _confirmDismiss(
+    DismissDirection direction,
+    GlobalKey<NavigatorState> navigator,
+  ) async {
+    if (direction != DismissDirection.startToEnd) {
+      return false;
+    }
+    return showDialog<bool>(
+      context: navigator.currentState.descendantContext,
+      builder: (BuildContext context) {
+        return ConfirmDialog(
+          title: const Text('Confirm'),
+          content: const Text('Are you sure you wish to delete this item?'),
+          okBtnChild:
+              const Text('DELETE', style: TextStyle(color: Colors.black)),
+          okBtnStyle: OutlinedButton.styleFrom(backgroundColor: Colors.red),
+          canBtnChild:
+              const Text('CANCEL', style: TextStyle(color: Colors.black)),
+        );
+      },
+    );
+  }
+
+  void _onDismissed(TodoController todoController, Todo todo) =>
+      todoController.deleteTodo(id: todo.id);
+
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    final todoState = watch(todoProvider.state);
+    final todoController = context.read(todoProvider);
+    final navigator = watch(navigatorKeyProvider);
     return ListView.builder(
       padding: const EdgeInsets.only(top: 20),
-      itemBuilder: (_, int index) => TodoTile(
-        todo: watch(todoProvider.state).todoList[index],
-      ),
-      itemCount: watch(todoProvider.state).todoList.length,
+      itemCount: todoState.todoList.length,
+      itemBuilder: (_, int index) {
+        final todo = todoState.todoList[index];
+        return Dismissible(
+          key: Key(todo.id.toString()),
+          confirmDismiss: (direction) => _confirmDismiss(
+            direction,
+            navigator,
+          ),
+          onDismissed: (direction) => _onDismissed(
+            todoController,
+            todo,
+          ),
+          background: const DismissBackground(
+            icon: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+            colors: Colors.red,
+            alignment: Alignment.centerLeft,
+          ),
+          secondaryBackground: Container(),
+          child: TodoTile(todo: todo),
+        );
+      },
     );
   }
 }
